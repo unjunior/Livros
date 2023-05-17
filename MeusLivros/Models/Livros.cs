aa88c9535595191c26e71736792a754adaeab6f7
Ubirajara Junior <unjunior4315@gmail.com>
2023-05-17 17:07:33 -0300

    Criado as models filmes e entity e os data anotations.

---
+++ MeusLivros/Models/Livros.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeusLivros.Models
+{
+    public class Livros : Entity
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        
+        public string Titulo { get; set; }
+        
+        [Required]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Codigo { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "O {0} é obrigatório")]
+        public string Genero { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Descricao { get; set; }
+        public bool Leitura { get; set; }
+    }
+}
